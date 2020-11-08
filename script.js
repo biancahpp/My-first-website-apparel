e8bc84a4d49e2bf062e55ad1c63d91bbd83a1a47
biancahpp <bianca_procopio@hotmail.com>
2020-11-08 16:08:29 -0600

    almost done with features

---
+++ script.js
@@ -0,0 +1,50 @@
+const submitButton = document.getElementById('submitButton');
+const email = document.querySelector('#email-input');
+const emailDiv = document.querySelector('#email');
+//let invalidEmail = false;
+
+function emailIsValid (event) {
+    let test = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value);
+    if (test) {
+        alert('Thank you for submitting your e-mail. Stay tuned!');
+    }
+    // else if (!email.value) {
+    //     if (!document.querySelector('.error')) {
+    //         let newImg = document.createElement('img');
+    //         newImg.src = "./images/icon-error.svg";
+    //         newImg.className = 'error';
+    //         document.getElementById('email').appendChild(newImg);
+    
+    //         let newDiv = document.createElement('div');
+    //         newDiv.innerText = 'Please provide an email';
+    //         newDiv.className = 'alert';
+    //         document.getElementById('text').appendChild(newDiv);
+
+    //         document.getElementById('email-input').style.border = "1px solid hsl(0, 93%, 68%)";
+    //         }
+    //}
+    else {
+        if (!document.querySelector('.error')) {
+        let newImg = document.createElement('img');
+        newImg.src = "./images/icon-error.svg";
+        newImg.className = 'error';
+        document.getElementById('email').appendChild(newImg);
+
+        let newDiv = document.createElement('div');
+        newDiv.innerText = 'Please provide a valid email';
+        newDiv.className = 'alert';
+        document.getElementById('text').appendChild(newDiv);
+        document.getElementById('email-input').style.border = "1px solid hsl(0, 93%, 68%)";
+        event.preventDefault()
+            }
+        }
+    }
+
+
+function submit () {
+
+}
+
+submitButton.addEventListener('click', emailIsValid);
+
+
